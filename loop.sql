e385739aa626d4b034f8a3480e52c3bf5e31282a
Чебан Ростислав <44871917+ChebanRostislavKM81@users.noreply.github.com>
2020-05-03 16:17:44 +0300

    Update loop.sql

--- loop.sql
+++ loop.sql
@@ -7,10 +7,14 @@ BEGIN
     
     FOR i IN 1..10 LOOP
         INSERT INTO cities (
+        dates,
         city,
+        country,
         tournament)
         VALUES (
+        01.01.70,
         trim(var_city) || i,
+        England,
         trim(var_tournament) || i);
         END LOOP;
     END;
